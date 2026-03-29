5ec8252a5e546993649193ad5f97993ed3124cbe
NavidUdc <nlobatos@unicartagena.edu.co>
2026-03-29 12:34:45 -0500

    feat: Creacion de arreglo y recorrdo del mismo

--- act.go
+++ act.go
@@ -9,6 +9,8 @@ type estudiante struct {
 }
 
 func main() {
+
+	fmt.Println("========== INICIALIZACION ==========")
 	var est = estudiante{nombre: "navid", edad: 5, promedio: 3.8}
 	var est1 = estudiante{"naser", 18, 4.0}
 	var est2 = estudiante{"naren", 25, 2.9}
@@ -16,4 +18,16 @@ func main() {
 	fmt.Println(est.nombre, est.edad, est.promedio)
 	fmt.Println(est1.nombre, est1.edad, est1.promedio)
 	fmt.Println(est2.nombre, est2.edad, est2.promedio)
+
+	fmt.Println("=========== RECORRIDO ===========")
+	var arreglo = []estudiante{
+		{"navid", 19, 4.5},
+		{"naser", 18, 3.0},
+		{"naren", 25, 2.3},
+	}
+
+	for i := 0; i < len(arreglo); i++ {
+		fmt.Println(arreglo[i].nombre, arreglo[i].edad, arreglo[i].promedio)
+	}
+
 }
--- act.js
+++ act.js
@@ -7,7 +7,7 @@ class estudiante{
 
 }
 
-
+console.log("================= INICIALIZACION =======================")
 let est = new estudiante("navid", 19, 4.0)
 let est1 = new estudiante("naser", 18, 3.7)
 let est2 = new estudiante("naren", 24, 4.3)
@@ -15,3 +15,14 @@ let est2 = new estudiante("naren", 24, 4.3)
 console.log(est)
 console.log(est1)
 console.log(est2)
+
+console.log("==================== RECORRIDO =========================")
+let arreglo= [
+    new estudiante("naser", 18, 3.5),
+    new estudiante("naren ", 24, 3.9),
+    new estudiante("navid", 19, 2.5)
+]
+
+for(let e of arreglo){
+    console.log(e.nombre, e.edad, e.promedio)
+}
