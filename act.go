2dd68211125fea1c516cc067884e793c2af23701
NavidUdc <nlobatos@unicartagena.edu.co>
2026-03-29 12:22:07 -0500

    feat: inicializacion de los objet0s

--- act.go
+++ act.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type estudiante struct {
 	nombre   string
 	edad     int
@@ -7,5 +9,11 @@ type estudiante struct {
 }
 
 func main() {
+	var est = estudiante{nombre: "navid", edad: 5, promedio: 3.8}
+	var est1 = estudiante{"naser", 18, 4.0}
+	var est2 = estudiante{"naren", 25, 2.9}
 
+	fmt.Println(est.nombre, est.edad, est.promedio)
+	fmt.Println(est1.nombre, est1.edad, est1.promedio)
+	fmt.Println(est2.nombre, est2.edad, est2.promedio)
 }
--- act.js
+++ act.js
@@ -7,3 +7,11 @@ class estudiante{
 
 }
 
+
+let est = new estudiante("navid", 19, 4.0)
+let est1 = new estudiante("naser", 18, 3.7)
+let est2 = new estudiante("naren", 24, 4.3)
+
+console.log(est)
+console.log(est1)
+console.log(est2)
