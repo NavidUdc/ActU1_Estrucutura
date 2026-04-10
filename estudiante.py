6ac23d848dade25fa9c820bd34af92666dc5421f
NavidUdc <nlobatos@unicartagena.edu.co>
2026-04-10 12:52:18 -0500

    feat: segunda parte, inicializacion en arreglo

--- estudiante.py
+++ estudiante.py
@@ -6,3 +6,11 @@ class Estudiante:
     
     def mostrarInfo(self):
         print(f"Nombre: {self.nombre} , Edad: {self.edad} , Promedio: {self.promedio}")
+
+#2. Inicializacion
+
+arreglo =[
+    Estudiante("Navid", 20, 3.8),
+    Estudiante("Naser", 18, 4.0),
+    Estudiante("Naren", 25, 2.9)
+]
